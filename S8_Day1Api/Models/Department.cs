ec0aa5b0f550577c1f07ee52333f89b19d56fc39
MahmoudAlshahaly <mahmoudahmed10197@gmail.com>
2025-03-10 20:52:05 +0200

    mooooooo

--- S8_Day1Api/Models/Department.cs
+++ S8_Day1Api/Models/Department.cs
@@ -3,9 +3,12 @@
     public class Department
     {
         public int Id { get; set; }
+        public int MyProperty3 { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public int MyProperty2 { get; set; }
         public ICollection<Employee>? Employees { get; set; }
         public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
     }
 }
